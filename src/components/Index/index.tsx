ebb991676d2f7f5768b0a564d1d9e65425985959
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-04-05 20:37:19 -0300

    tailwind configured

--- src/components/Index/index.tsx
+++ src/components/Index/index.tsx
@@ -1,7 +1,7 @@
 import Logo from 'components/Logo/logo'
 
 const Index = () => {
-    return <Logo />
+    return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 
 export default Index
