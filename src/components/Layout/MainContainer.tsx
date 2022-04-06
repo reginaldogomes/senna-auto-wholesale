982b3e2a82ae48ec897b407ec0ab911bac81f93c
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-04-06 12:27:54 -0300

    layout component created

---
+++ src/components/Layout/MainContainer.tsx
@@ -0,0 +1,21 @@
+import { Footer } from './Footer'
+import { Header } from './Header'
+
+interface LayoutProps {
+    title: string
+    children: React.ReactNode
+}
+
+export default function MainContainer(props: LayoutProps) {
+    return (
+        <>
+            <Header />
+            <div className={`flex-col justify-center items-center bg-black`}>
+                <div className={`flex justify-center items-center py-10`}>
+                    <main>{props.children}</main>
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
