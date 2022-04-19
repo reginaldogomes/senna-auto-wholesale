409678ac5a36db165731083ce06cae44b3748a49
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-04-19 10:32:40 -0300

    header component adjusted props

--- src/components/Layout/HeadingPage.tsx
+++ src/components/Layout/HeadingPage.tsx
@@ -1,10 +1,11 @@
 interface HeadingPageProps {
     title: string
 }
+
 export const HeadingPage = (props: HeadingPageProps) => {
     return (
         <div className="w-full bg-gray-100">
-            <div className="container mx-auto h-50 text-black py-12">
+            <div className="container mx-auto h-50 text-black py-12 ">
                 <h1 className="text-4xl">{props.title}</h1>
             </div>
         </div>
