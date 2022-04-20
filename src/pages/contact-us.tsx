a928414b99948c9b3babbbcd00586fd5fff36ae8
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-04-19 22:20:22 -0300

    headind page adjusted

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -1,99 +1,103 @@
 import type { NextPage } from 'next'
+import { HeadingPage } from 'components/Layout/HeadingPage'
 
 const ContactUs: NextPage = () => {
     return (
-        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-md">
-            <form>
-                <div className="form-group mb-6">
-                    <input
-                        type="text"
-                        className="form-control block
-        w-full
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                        id="exampleInput7"
-                        placeholder="Name"
-                    ></input>
-                </div>
-                <div className="form-group mb-6">
-                    <input
-                        type="email"
-                        className="form-control block
-        w-full
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                        id="exampleInput8"
-                        placeholder="Email address"
-                    ></input>
-                </div>
-                <div className="form-group mb-6">
-                    <textarea
+        <>
+            <HeadingPage title={'Sell'} />
+            <div className="block p-6 rounded-lg shadow-lg bg-white max-w-md">
+                <form>
+                    <div className="form-group mb-6">
+                        <input
+                            type="text"
+                            className="form-control block
+    w-full
+    px-3
+    py-1.5
+    text-base
+    font-normal
+    text-gray-700
+    bg-white bg-clip-padding
+    border border-solid border-gray-300
+    rounded
+    transition
+    ease-in-out
+    m-0
+    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            id="exampleInput7"
+                            placeholder="Name"
+                        ></input>
+                    </div>
+                    <div className="form-group mb-6">
+                        <input
+                            type="email"
+                            className="form-control block
+    w-full
+    px-3
+    py-1.5
+    text-base
+    font-normal
+    text-gray-700
+    bg-white bg-clip-padding
+    border border-solid border-gray-300
+    rounded
+    transition
+    ease-in-out
+    m-0
+    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            id="exampleInput8"
+                            placeholder="Email address"
+                        ></input>
+                    </div>
+                    <div className="form-group mb-6">
+                        <textarea
+                            className="
+    form-control
+    block
+    w-full
+    px-3
+    py-1.5
+    text-base
+    font-normal
+    text-gray-700
+    bg-white bg-clip-padding
+    border border-solid border-gray-300
+    rounded
+    transition
+    ease-in-out
+    m-0
+    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+  "
+                            id="exampleFormControlTextarea13"
+                            placeholder="Message"
+                        ></textarea>
+                    </div>
+                    <button
+                        type="submit"
                         className="
-        form-control
-        block
-        w-full
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-      "
-                        id="exampleFormControlTextarea13"
-                        placeholder="Message"
-                    ></textarea>
-                </div>
-                <button
-                    type="submit"
-                    className="
-      w-full
-      px-6
-      py-2.5
-      bg-blue-600
-      text-white
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      rounded
-      shadow-md
-      hover:bg-blue-700 hover:shadow-lg
-      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-      active:bg-blue-800 active:shadow-lg
-      transition
-      duration-150
-      ease-in-out"
-                >
-                    Send
-                </button>
-            </form>
-        </div>
+  w-full
+  px-6
+  py-2.5
+  bg-blue-600
+  text-white
+  font-medium
+  text-xs
+  leading-tight
+  uppercase
+  rounded
+  shadow-md
+  hover:bg-blue-700 hover:shadow-lg
+  focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+  active:bg-blue-800 active:shadow-lg
+  transition
+  duration-150
+  ease-in-out"
+                    >
+                        Send
+                    </button>
+                </form>
+            </div>
+        </>
     )
 }
 
