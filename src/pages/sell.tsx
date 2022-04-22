594c3408ab983398991ebe51f64c4c65a459fdb0
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-04-22 18:54:44 -0300

    form sell adjusted layout page

--- src/pages/sell.tsx
+++ src/pages/sell.tsx
@@ -6,16 +6,10 @@ import { SellForm } from 'components/SellForm/SellForm'
 const Sell: NextPage = () => {
     return (
         <>
-            <HeadingPage title={'Sell'} />{' '}
+            <HeadingPage title={'Sell'} />
             <div className="flex flex-col flex-wrap">
-                <section className="container m-auto bg-gray-300 py-16">
+                <section className="container m-auto py-16">
                     <SellForm />
-                </section>
-                <section className="container m-auto bg-gray-200 py-24">
-                    <h1>seção 2</h1>
-                </section>
-                <section className="container m-auto bg-gray-100 py-60">
-                    <h1>seção 3</h1>
                 </section>
             </div>
         </>
