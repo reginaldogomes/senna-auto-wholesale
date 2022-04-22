77dca66adbef093fdd01717b814bb7f06e781d9a
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-04-22 18:46:01 -0300

    icons removed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,6 @@ import type { AppProps } from 'next/app'
 import '../styles/global.css'
 import MainContainer from 'components/Layout/MainContainer'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-library.add(fab, faCoffee)
-
 function App({ Component, pageProps }: AppProps) {
     return (
         <MainContainer title="Senna Auto Wholesale">
