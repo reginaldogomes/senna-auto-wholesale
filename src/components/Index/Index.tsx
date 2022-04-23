9383a91a1c148bde5ef184435773f7faf7e789e1
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-04-23 18:57:14 -0300

    index component adjusted layout

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -1,15 +1,17 @@
-import { Slide } from 'components/common/Slide/Index'
+import { Title } from 'components/common/Title/Index'
 
 const Index = () => {
     return (
         <>
-            <Slide />
-            <div className="flex flex-col flex-wrap">
-                <section className="container m-auto bg-gray-300 py-16">
-                    <h1>seção 1</h1>
+            <div className="w-full flex flex-col flex-wrap">
+                <section className="container m-auto py-16">
+                    <div className="flex mb-4">
+                        <div className="w-1/2 bg-gray-400 h-12"></div>
+                        <div className="w-1/2 bg-gray-500 h-12"></div>
+                    </div>
                 </section>
                 <section className="container m-auto bg-gray-200 py-24">
-                    <h1>seção 2</h1>
+                    <Title title="About Us" />
                 </section>
                 <section className="container m-auto bg-gray-100 py-60">
                     <h1>seção 3</h1>
