912ae95c0fda9e2aa30d4b193347c2714e2dea9b
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-05-10 19:46:46 -0300

    ajustes form sell

--- src/components/SellForm/SellForm.tsx
+++ src/components/SellForm/SellForm.tsx
@@ -2,14 +2,28 @@ import { Button } from 'components/common/Button/Index'
 
 export const SellForm = () => {
     return (
-        <div className="block p-6 rounded-lg shadow-lg bg-gray-100 mx-auto">
-            <form>
+        <div className="flex flex-col max-w-5xl m-auto">
+            <h3 className="text-2xl text-blue text-center">
+                <strong>Get an instant cash offer on your car</strong>
+            </h3>
+            <p className="text-blue mt-4 text-center">
+                Tell us about your car to receive an offer.
+            </p>
+            <form className="bg-yellow bg-opacity-5 py-10 mt-10 rounded-lg p-10">
                 <div className="form-group mb-6">
                     <input
                         type="text"
                         className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         id="exampleInput7"
                         placeholder="VIN"
+                    ></input>
+                </div>
+                <div className="form-group mb-6">
+                    <input
+                        type="email"
+                        className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        id="exampleInput8"
+                        placeholder="Province"
                     ></input>
                 </div>
                 <div className="form-group mb-6">
