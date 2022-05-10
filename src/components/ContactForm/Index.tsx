8ea683ba4eb2e9f843319f4f8640c2d2b01e6ba3
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-05-09 22:07:58 -0300

    novos ajustes

--- src/components/ContactForm/Index.tsx
+++ src/components/ContactForm/Index.tsx
@@ -8,10 +8,11 @@ export const ContactForm = () => {
             </h3>
             <p className="text-blue mt-4 text-center">
                 If you can’t find what you need, please submit a question
+                <br />
                 through our contact form below & we’ll get back to you as soon
                 as possible.
             </p>
-            <form className="bg-yellow opacity-5 py-10 mt-10 rounded-lg p-10">
+            <form className="bg-yellow bg-opacity-5 py-10 mt-10 rounded-lg p-10">
                 <div className="form-group mb-6">
                     <input
                         type="text"
@@ -34,6 +35,43 @@ export const ContactForm = () => {
                         id="exampleFormControlTextarea13"
                         placeholder="Message"
                     ></textarea>
+                </div>
+                <div className="form-group mb-6">
+                    <div className="flex justify-center">
+                        <div className="mb-3 xl:w-96">
+                            <select
+                                className="form-select form-select-lg mb-3
+      appearance-none
+      block
+      w-full
+      px-4
+      py-2
+      text-xl
+      font-normal
+      text-gray-700
+      bg-white bg-clip-padding bg-no-repeat
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                aria-label=".form-select-lg example"
+                            >
+                                <option selected>Open this select menu</option>
+                                <option value="1">
+                                    General (How Clutch Works, Payment Methods)
+                                </option>
+                                <option value="2">
+                                    More information about a Clutch Vehicle
+                                </option>
+                                <option value="3">
+                                    Post Purchase Support/90-Day Guarantee
+                                    Claims
+                                </option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
                 <Button title="Send Message" />
             </form>
