f7e41e523037471ed5985f31572b132e75c314e7
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-05-24 22:02:31 -0300

    about us adjusted

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -7,48 +7,52 @@ import { FiMail } from 'react-icons/fi'
 const Index = () => {
     return (
         <>
-            <div className="w-full flex flex-col flex-wrap">
+            <div className="flex flex-col flex-wrap">
                 <section className="container m-auto">
-                    <div className="container mb-4">
-                        <div className="flex flex-wrap lg:flex-nowrap">
-                            <div className="lg:w-1/2 py-5">
-                                <img
-                                    src="images/sell-your-car-web.jpg"
-                                    className="max-w-full h-auto rounded-lg"
-                                    alt="..."
-                                />
-                            </div>
-                            <div className="bg-white pt-8 px-8">
-                                <h1 className="text-5xl text-center text-blue lg:text-7xl lg:text-left">
-                                    Sell <strong>your</strong> car
-                                </h1>
-                                <h2 className="text-2xl text-center text-blue lg:text-4xl lg:text-left pt-4">
-                                    <strong>Get</strong> a free
-                                    <strong> appraisal</strong> in minutes
-                                </h2>
-                                <Link href="/sell">
-                                    <a className="inline-block w-full lg:max-w-xs text-center mt-5 px-7 py-3 bg-yellow text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
-                                        appraisal
-                                    </a>
-                                </Link>
-                            </div>
+                    <div className="flex flex-wrap lg:flex-nowrap">
+                        <div className="lg:w-1/2 py-5">
+                            <img
+                                src="images/sell-your-car-web.jpg"
+                                className="max-w-full h-auto rounded-lg"
+                                alt="..."
+                            />
+                        </div>
+                        <div className="bg-white pt-8 px-8">
+                            <h1 className="text-5xl text-center text-blue lg:text-7xl lg:text-left">
+                                Sell <strong>your</strong> car
+                            </h1>
+                            <h2 className="text-2xl text-center text-blue lg:text-4xl lg:text-left pt-4">
+                                <strong>Get</strong> a free
+                                <strong> appraisal</strong> in minutes
+                            </h2>
+                            <Link href="/sell">
+                                <a className="inline-block w-full lg:max-w-xs text-center mt-5 px-7 py-3 bg-yellow text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                                    appraisal
+                                </a>
+                            </Link>
                         </div>
                     </div>
                 </section>
                 <section className="container m-auto text-blue bg-yellow bg-opacity-5 p-6">
                     <Title title="About Us" />
                     <p className="mt-5 sm:text-center">
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the industrys
-                        standard dummy text ever since the 1500s, when an
-                        unknown printer took a galley of type and scrambled it
-                        to make a type specimen book. It has survived not only
-                        five centuries, but also the leap into electronic
-                        typesetting, remaining essentially unchanged. It was
-                        popularised in the 1960s with the release of Letraset
-                        sheets containing Lorem Ipsum passages, and more
-                        recently with desktop publishing software like Aldus
-                        PageMaker including versions of Lorem Ipsum.
+                        The British Columbia Most Trusted Auto Wholesaler
+                    </p>
+                    <p className="mt-5 sm:text-center">
+                        No one likes to waste time and energy on any
+                        transaction. After all, time really is money when it
+                        comes to selling your vehicle at the fairest price
+                        possible. That’s why Senna Auto Wholesale is so driven
+                        to deliver top-notch customer service for every
+                        automobile we purchase—from luxury or exotic cars to
+                        middle-market vehicles.
+                    </p>
+                    <p className="mt-5 sm:text-center">
+                        As a British Columbia licensed, insured automobile
+                        wholesaler, Senna Auto Wholesale guarantees a fair and
+                        smooth transaction every time, with knowledgeable
+                        professionals who are driven to deliver superior
+                        customer service. Our reputation is built on it.
                     </p>
                 </section>
                 <section className="container m-auto bg-yellow bg-opacity-5 p-6 mt-10">
