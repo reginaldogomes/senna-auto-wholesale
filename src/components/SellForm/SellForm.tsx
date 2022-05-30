56fc3ec71d4bf2eb0012c9de70ba18a4681296b0
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-05-29 21:14:58 -0300

    update sell form

--- src/components/SellForm/SellForm.tsx
+++ src/components/SellForm/SellForm.tsx
@@ -32,7 +32,7 @@ export const SellForm = () => {
                             id="countries"
                             className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         >
-                            <option selected>Choose a country</option>
+                            <option selected></option>
                             <option value="US">United States</option>
                             <option value="CA">Canada</option>
                             <option value="FR">France</option>
@@ -53,12 +53,15 @@ export const SellForm = () => {
                     </label>
                 </div>
                 <div className="form-group mb-6">
-                    <span>Select your transmission type</span>
+                    <span className="text-gray-700 text-2xl font-normal">
+                        Select your transmission type
+                    </span>
                     <div className="form-check">
                         <label>
                             <span>Automatic</span>
                             <input
-                                className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                name="transmission"
+                                className="rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                                 type="radio"
                             ></input>
                         </label>
@@ -67,28 +70,79 @@ export const SellForm = () => {
                         <label>
                             <span>Manual</span>
                             <input
-                                className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                name="transmission"
+                                className="rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                                 type="radio"
                             ></input>
                         </label>
                     </div>
                 </div>
                 <div className="form-group mb-6">
-                    <span>Select your transmission type</span>
+                    <span className="text-gray-700 text-2xl font-normal">
+                        Do you own the vehicle outright?
+                    </span>
+                    <div className="flex space-x-5">
+                        <label className="block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
+                            <span>Yes</span>
+                            <input
+                                name="vehicle"
+                                className="rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                type="radio"
+                            ></input>
+                        </label>
+                        <label className="block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
+                            <span>No, it´s financed</span>
+                            <input
+                                name="vehicle"
+                                className="rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                type="radio"
+                            ></input>
+                        </label>
+                        <label className="block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none">
+                            <span>No, it´s leased</span>
+                            <input
+                                name="vehicle"
+                                className="rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                type="radio"
+                            ></input>
+                        </label>
+                    </div>
+                </div>
+                <div className="form-group mb-6">
+                    <label>
+                        <span className="text-gray-700 text-2xl font-normal">
+                            Select vehicle colour
+                        </span>
+                        <select
+                            id="countries"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        >
+                            <option selected></option>
+                            <option value="US">Blue</option>
+                            <option value="CA">Black</option>
+                            <option value="FR">White</option>
+                            <option value="DE">Yellow</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="form-group mb-6">
+                    <span>Are you selling or trading in?</span>
                     <div className="form-check">
                         <label>
-                            <span>Automatic</span>
+                            <span>Selling</span>
                             <input
-                                className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                name="vehicle"
+                                className="rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                                 type="radio"
                             ></input>
                         </label>
                     </div>
                     <div className="form-check">
                         <label>
-                            <span>Manual</span>
+                            <span>Trading in</span>
                             <input
-                                className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                name="vehicle"
+                                className="rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                                 type="radio"
                             ></input>
                         </label>
@@ -97,7 +151,7 @@ export const SellForm = () => {
                 <div className="form-group mb-6">
                     <label>
                         <span className="text-gray-700 text-base font-normal">
-                            Province
+                            Do you own the vehicle outright?
                         </span>
                         <input
                             type="email"
