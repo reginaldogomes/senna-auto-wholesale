152e00d7cbbe7cd3076826580702b0e37e49f63c
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-06-09 20:25:30 -0300

    button component ajusted

--- src/components/common/Button/Index.tsx
+++ src/components/common/Button/Index.tsx
@@ -1,11 +1,9 @@
 interface ButtonProps {
     title: string
-    state: boolean
 }
 export const Button = (props: ButtonProps) => {
     return (
         <button
-            disabled={props.state}
             type="submit"
             className="inline-block px-6 py-2.5 bg-yellow text-blue font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
         >
