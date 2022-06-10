b9b32cbafbd73c466a58a97f4ba736deb34e18be
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2022-06-09 23:35:44 -0300

    button adjusted

--- src/components/common/Button/Index.tsx
+++ src/components/common/Button/Index.tsx
@@ -5,7 +5,7 @@ export const Button = (props: ButtonProps) => {
     return (
         <button
             type="submit"
-            className="inline-block px-6 py-2.5 bg-yellow text-blue font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            className="w-full inline-block px-6 py-2.5 bg-yellow text-blue font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
         >
             {props.title}
         </button>
